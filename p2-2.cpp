13b59eea900f48fb1a621318cb6d243cd2fda650
mehdifeshki <mahdifeshki08@gmail.com>
2025-04-13 13:39:04 +0330

    Update and rename p2-2 to p2-2.cpp

---
+++ p2-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class students {
+private:
+string firstname , lastname;
+int age ;
+
+//مقدار دهی اولیه
+public:
+students():firstname("") , lastname("") , age(0){}
